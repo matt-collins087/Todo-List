0b370c0df3318f6076a8271d4c7bd64ff7a0ce01
Matt <matt.collins087@gmail.com>
2021-07-09 15:29:42 -0500

    finished todo list

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
 let newItems = [];
+let workItems = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  let today = new Date();
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  };
-  let day = today.toLocaleDateString('en-US', options);
 
-  res.render('list', {kindOfDay: day, newListItem: newItems})
+  let day = date.getDate();
+  res.render('list', {listTitle: day, newListItem: newItems})
 });
 
 app.post('/', (req, res) => {
-  newItems.push(req.body.newItem);
+  let item = req.body.newItem;
 
-  res.redirect('/');
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    newItems.push(item);
+    res.redirect('/');
+  }
+});
+
+app.get('/work', (req, res) => {
+  res.render('list', {listTitle: 'Work List', newListItem: workItems})
+});
+
+app.post('/work', (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 
